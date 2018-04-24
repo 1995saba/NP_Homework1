041cd19c12f5778ea3001aeb585479962ad64add
Sabina <sabina.orunbaeva@gmail.com>
2018-04-24 14:15:19 +0600

    Add files via upload

---
+++ Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port;
+            Console.WriteLine("Введите номер порта: ");
+            string portAsString = Console.ReadLine();
+            Int32.TryParse(portAsString, out port);
+
+            IPAddress serverAddr;
+            Console.WriteLine("Введите IP адрес: ");
+            string ipAddressAsString = Console.ReadLine();
+            IPAddress.TryParse(ipAddressAsString, out serverAddr);
+
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(serverAddr, port);
+
+                byte[] data = new byte[256];
+                StringBuilder response = new StringBuilder();
+                NetworkStream stream = client.GetStream();
+
+                do
+                {
+                    int bytes = stream.Read(data, 0, data.Length);
+                    response.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                }
+                while (stream.DataAvailable);
+
+                Console.WriteLine(response.ToString());
+
+                stream.Close();
+                client.Close();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+
+            Console.WriteLine("Запрос завершен...");
+            Console.Read();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+
+            int port;
+            Console.WriteLine("Введите номер порта: ");
+            string portAsString = Console.ReadLine();
+            Int32.TryParse(portAsString, out port);
+            
+            IPAddress localAddr;
+            Console.WriteLine("Введите IP адрес: ");
+            string ipAddressAsString = Console.ReadLine();
+            IPAddress.TryParse(ipAddressAsString, out localAddr);
+
+            try
+            {
+                server = new TcpListener(localAddr, port);
+                server.Start();
+
+                var client = server.AcceptTcpClient();
+                byte[] buffer = new byte[10];
+                StringBuilder globalBuffer = new StringBuilder();
+
+                while (true)
+                {
+                    var stream = client.GetStream();
+                    for (int i = 0; i < stream.Length; i++)
+                    {
+                        globalBuffer.Append(Convert.ToChar(buffer[i]));
+                    }
+
+                    string response = globalBuffer.ToString();
+                    byte[] data = Encoding.UTF8.GetBytes(response);
+
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Отправлено сообщение: {0}", response);
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+            }
+        }
+    }
+}
